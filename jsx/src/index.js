a32f9644e8d1c5a00b928bccebfba5d59497a3f4
Sidney de Geus <sidneydegeus@hotmail.com>
2020-09-08 14:33:19 +0200

    Simple display of some html with js using react

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,17 +1,30 @@
+// Import the React and ReactDom libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
+function getButtonText() {
+    return "Click on me!";
+}
+
+// Create a react component
+const App = function () { // = () => {
+    const buttonText = 'Click Me!';
+
+    return (
+        <div> 
+            <label className="label" for="name">
+                Enter Name:
+                </label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>
+                {getButtonText}
+            </button>
+        </div> 
+        );
+}
+
+// Take the react component and show it on the screen
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />,
+    document.querySelector('#root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
